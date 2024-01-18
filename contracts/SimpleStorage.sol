b8b52a209e3b7a05e6f6f6bec12ddd093032b604
Paras Puneet Singh <136245940+ParasPuneetSingh@users.noreply.github.com>
2024-01-18 23:16:12 +0530

    Update SimpleStorage.sol
    changed data type to string

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,13 +3,13 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage{
-    uint256 public data;
+    string public data;
 
-        function set(uint256 value) public{
+        function set(string memory value) public{
             data = value;
         }
         
-        function get() public view returns (uint256) {
+        function get() public view returns (string memory) {
             return data;
         }
         
