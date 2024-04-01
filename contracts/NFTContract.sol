ca48c9862fcea837135505b61580b61881af261c
Shriyam-Avasthi <avasthishriyam@gmail.com>
2024-04-01 15:58:42 +0530

    Update NFTContract.sol

--- contracts/NFTContract.sol
+++ contracts/NFTContract.sol
@@ -6,13 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract NFTContract is ERC721 {
 
     uint256 private _tokenIds = 0;
-    uint256 private _totalMinted = 0;
-
-    string[]  private tokenURIs;
     uint256 public PRICE_PER_TOKEN = 0.01 ether;
-
-    uint256 public LIMIT_PER_ADDRESS = 5;
-    uint256 public MAX_SUPPLY  = 5;
 
     constructor() ERC721("NFTContract", "NFT")
     {}
@@ -22,11 +16,11 @@ contract NFTContract is ERC721 {
         external
         returns (uint256)
     {
+        require(PRICE_PER_TOKEN <= msg.value, "Ether paid is incorrect");
         uint256 newItemId = _tokenIds;
         _safeMint(owner, newItemId);
 
         _tokenIds++;
-        _totalMinted++;
         return newItemId;
     }
 }
